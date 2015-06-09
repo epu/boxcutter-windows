7796859b727046a400fca312209b4ef38abfe8ed
Erik Michael Purins <erik.purins@imgtec.com>
2015-06-09 11:05:03 -0700

    Renumber net/posh commands to workaround strange runtime naming.
     * Using names with few dashes and under 13 characters to work around
       bad generated file names from 00-run*cmd, so they occur in order.
     * Pass iso checksum type down in json also. Seems to still checksum when set.
       - Maybe it needs to unset checksum value if set?
    
    Confirmed in test-kitchen of local project that image comes up with posh4 and net4.5 - posh4 is notoriously hard to correctly install, since any sub-patch returns success for the entire windows update applied. It also requires exactly .net 4.5 rtm.

--- floppy/yy02-net45.cmd
+++ floppy/yy02-net45.cmd
@@ -4,10 +4,11 @@
 @if defined PACKER_DEBUG (@echo on) else (@echo off)
 @set _SILENT_=@
 %_SILENT_%if defined PACKER_DEBUG set _SILENT_=
-
+::%_SILENT_%set PATH=%ChocolateyInstall%\bin;%PATH%
+%_SILENT_%where choco > NUL 2>&1 || set PATH=%ProgramData%\chocolatey\bin;%PATH%
 %_SILENT_%title Installing .NET framework 4.5. Please wait...
 %_SILENT_%echo ==^> Installing .NET 4.5.
-%_SILENT_%set INSTALL_COMMAND=choco install -y dotnet45
+%_SILENT_%set INSTALL_COMMAND=choco install -y dotnet4.5
 %_SILENT_%%INSTALL_COMMAND%
 %_SILENT_%set CODE=%ERRORLEVEL%
 %_SILENT_%if "%CODE%x" EQU "0x" goto :exit
